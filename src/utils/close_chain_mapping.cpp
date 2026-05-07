6a6e5c9bb7621af6133a6e20a1eea21443b49a4b
Luo1imasi <2488288779@qq.com>
2026-05-07 11:28:41 +0800

    fix params

--- src/utils/close_chain_mapping.cpp
+++ src/utils/close_chain_mapping.cpp
@@ -3,17 +3,13 @@
 
 std::shared_ptr<Decouple> Decouple::create(const std::string &type)
 {
-    if (type == "rp1")
-    {
-        return std::make_shared<DecoupleRP1>();
-    }
-    else if (type == "atom01")
+    if (type == "atom01")
     {
         return std::make_shared<DecoupleAtom01>();
     }
     else
     {
         throw std::runtime_error("Unknown close_chain type: " + type +
-                                 ". Supported types: rp1, atom01");
+                                 ". Supported types: atom01");
     }
 }
